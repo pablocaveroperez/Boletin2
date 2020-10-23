9b797e22012602f89497ab20a768fcf4ad8fd319
pabcavper <gelves.mula@gmail.com>
2020-10-23 11:20:12 +0200

    Creacion de los 5 hilos

--- Ejercicio1/src/EJ_1.java
+++ Ejercicio1/src/EJ_1.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class EJ_1 {
+    public static void main(String[] args) {
+        final byte NUMTHREADS = 5;
+
+        ArrayList<Thread> listThreads = new ArrayList<Thread>();
+        ArrayList<Byte> byteArrayList = new ArrayList<>();
+
+        // Creacion de los Threads
+        for (byte i = 0; i < NUMTHREADS; i++) {
+            System.out.println("Creando el hilo " + i);
+            listThreads.add(new Thread(new Hilo()));
+        }
+    }
 }
---
+++ Ejercicio1/src/Hilo.java
@@ -0,0 +1,7 @@
+public class Hilo implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
