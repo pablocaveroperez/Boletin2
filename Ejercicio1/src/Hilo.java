9337ff163b43071e3a8db28e83565762f33f34d6
pabcavper <gelves.mula@gmail.com>
2020-10-23 12:05:41 +0200

    Creado el constructor de la clase Hilo

--- Ejercicio1/src/Hilo.java
+++ Ejercicio1/src/Hilo.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 public class Hilo implements Runnable {
+
+    ArrayList<Byte> byteArrayList = new ArrayList<>();
+
+    public Hilo (ArrayList<Byte> byteArrayList) {
+        this.byteArrayList = byteArrayList;
+    }
 
     @Override
     public void run() {
