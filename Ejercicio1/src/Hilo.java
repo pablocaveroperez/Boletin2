60e4899262ff3a85d08328419a33e8cb85e5b669
pabcavper <gelves.mula@gmail.com>
2020-10-23 12:21:20 +0200

    Añadidos los getters y setters de la clase Hilo

--- Ejercicio1/src/Hilo.java
+++ Ejercicio1/src/Hilo.java
@@ -1,15 +1,36 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Hilo implements Runnable {
-
-    ArrayList<Byte> byteArrayList = new ArrayList<>();
+    private final byte bMAXNUM = 50;
+    private final byte bMINNUM = -50;
+    private byte iNum;
+    private ArrayList<Byte> byteArrayList = new ArrayList<>();
 
     public Hilo (ArrayList<Byte> byteArrayList) {
         this.byteArrayList = byteArrayList;
     }
 
+    public byte getbMAXNUM() {
+        return bMAXNUM;
+    }
+
+    public byte getbMINNUM() {
+        return bMINNUM;
+    }
+
+    public byte getiNum() {
+        return iNum;
+    }
+
+    public ArrayList<Byte> getByteArrayList() {
+        return byteArrayList;
+    }
+
     @Override
     public void run() {
+        Random r = new Random();
+        iNum = (byte) (bMINNUM + r.nextInt(bMAXNUM - bMINNUM + 1));
 
     }
 }
