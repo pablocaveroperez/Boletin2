2959600aa71879c1d7c27e279fe4f4883663cdc4
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-10-26 17:50:58 +0100

    Constructor de la clase Hilo3

--- Ejercicio4/src/Hilo3.java
+++ Ejercicio4/src/Hilo3.java
@@ -1,2 +1,12 @@
-public class Hilo3 {
+public class Hilo3 implements Runnable {
+    private double dNumero;
+
+    public Hilo3(double dNumero) {
+        this.dNumero = dNumero;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
