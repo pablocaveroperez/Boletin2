f1995760ab2e18db326f8b1210f612d672712bc1
pabcavper <gelves.mula@gmail.com>
2020-10-26 13:50:50 +0100

    En pogreso

--- Ejercicio3/src/EJ_3.java
+++ Ejercicio3/src/EJ_3.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class EJ_3 {
+
     public static void main(String[] args) {
+        final byte ARRAY_SIZE = 10;
+        final byte NUMTHREADS = 5;
+        ArrayList<Hilo> listRunnable = new ArrayList<>();
+        byte[] elementos = new byte[ARRAY_SIZE];
+
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            elementos[i] = 0;
+        }
+
+        for (int i = 0; i < NUMTHREADS; i++) {
+            listRunnable.add(new Hilo());
+        }
+
+
+
 
     }
 }
--- Ejercicio3/src/Hilo.java
+++ Ejercicio3/src/Hilo.java
@@ -1,2 +1,14 @@
-public class Hilo {
+public class Hilo implements Runnable {
+    final byte ARRAY_SIZE = 10;
+    private byte[] elementos = new byte[ARRAY_SIZE];
+
+    public Hilo() {
+
+    }
+
+
+    @Override
+    public void run() {
+
+    }
 }
