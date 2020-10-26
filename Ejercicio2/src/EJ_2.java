08913f13c04f62ce32424cbb72a2ca79762e64b3
pabcavper <gelves.mula@gmail.com>
2020-10-26 13:30:16 +0100

    Terminado el Ejercicio 2 y la clase EJ_2

--- Ejercicio2/src/EJ_2.java
+++ Ejercicio2/src/EJ_2.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class EJ_2 {
     public static void main(String[] args) {
+        Hilo hilo1 = new Hilo((byte) 0);
+        Hilo hilo2 = new Hilo((byte) 1);
+        Hilo hilo3 = new Hilo((byte) 2);
+        ArrayList<Thread> listThreads = new ArrayList<>();
+        listThreads.add(new Thread(hilo1));
+        listThreads.add(new Thread(hilo2));
+        listThreads.add(new Thread(hilo3));
 
+        for (Thread thread : listThreads) {
+            thread.start();
+        }
+
+        for (Thread thread : listThreads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
