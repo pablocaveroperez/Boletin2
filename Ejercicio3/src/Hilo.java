183ea64bb38d55fcefea25e691a4caa67dde8b64
pabcavper <gelves.mula@gmail.com>
2020-10-26 13:57:41 +0100

    Terminada la clase Hilo

--- Ejercicio3/src/Hilo.java
+++ Ejercicio3/src/Hilo.java
@@ -1,14 +1,29 @@
+import java.util.Random;
+
 public class Hilo implements Runnable {
-    final byte ARRAY_SIZE = 10;
+    private final byte ARRAY_SIZE = 10;
     private byte[] elementos = new byte[ARRAY_SIZE];
+    private final byte bMINNUM = -50;
+    private final byte bMAXNUM = 50;
 
     public Hilo() {
 
     }
 
+    public byte[] getElementos() {
+        return elementos;
+    }
+
+    public void setElementos(byte[] elementos) {
+        this.elementos = elementos;
+    }
+
 
     @Override
     public void run() {
-
+        Random r = new Random();
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            elementos[i] = (byte) (bMINNUM + r.nextInt(bMAXNUM - bMINNUM + 1));
+        }
     }
 }
