d4748f6e474b4f6eca4fa36104101bc717fafc7a
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-10-26 17:53:16 +0100

    Terminada la clase Hilo3

--- Ejercicio4/src/Hilo3.java
+++ Ejercicio4/src/Hilo3.java
@@ -1,5 +1,6 @@
 public class Hilo3 implements Runnable {
     private double dNumero;
+    private final double DIEZ = 10.0;
 
     public Hilo3(double dNumero) {
         this.dNumero = dNumero;
@@ -7,6 +8,7 @@ public class Hilo3 implements Runnable {
 
     @Override
     public void run() {
-
+        double dResultado = dNumero / DIEZ;
+        System.out.println("El resultado de dividir " + dNumero + " entre " + DIEZ + " es: " + dResultado);
     }
 }
