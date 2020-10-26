b4a88f256537f34779bafdc9a189767c2fb5b262
pabcavper <gelves.mula@gmail.com>
2020-10-26 12:58:26 +0100

    Declarados los metodos de la clase Hilo

--- Ejercicio2/src/Hilo.java
+++ Ejercicio2/src/Hilo.java
@@ -1,2 +1,26 @@
-public class Hilo {
+public class Hilo implements Runnable {
+    private byte bId;
+    public Hilo (byte bId) {
+        this.bId = bId;
+    }
+
+    @Override
+    public void run() {
+        if (bId == 0)
+            bucle100();
+        else if (bId == 1)
+            product2Arrays();
+        else if (bId == 2)
+            readNPrint();
+    }
+
+    private void readNPrint() {
+    }
+
+    private void product2Arrays() {
+    }
+
+    private void bucle100() {
+
+    }
 }
